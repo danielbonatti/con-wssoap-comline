fbb137ea9e16ce5c5024a75366fe88ce0b774cf8
danielbonatti <bonattidaniel@hotmail.com>
2023-06-02 14:47:45 -0300

    ver final

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
 
 $sUrl = $argv[1]; // Link
-$sXml = $argv[2]; // Arquivo xml em formato texto
+$sArq = $argv[2]; // Arquivo (xml/json) em formato texto
 $sMet = $argv[3]; // Método a ser envocado
 
-$client = new SoapClient($sUrl);
+$nTpc = $argv[4] ?? '1'; // Tipo de conteúdo. 1 => string to xml / 2 => string to json
 
-$xml = simplexml_load_string($sXml);
+//trace: habilitar as funções de debug (todas iniciam com "__").
+//Exceptions: ativará que os erros serão lançados via exceptions (SoapFault)
+//cache_wsdl: com o parâmetro WSDL_CACHE_NONE, não haverá cache.
+$client = new SoapClient(
+    $sUrl,
+    array( 
+        'trace' => 1, 
+        'exceptions' => true, 
+        'cache_wsdl' => WSDL_CACHE_NONE
+    ) 
+);
+
+if ($nTpc == '1') {
+    $con = simplexml_load_string($sArq);
+} else {
+    $con = json_decode($sArq,TRUE);
+}
 
 try {
-    $result = $client->$sMet($xml);
+    $result = $client->$sMet($con);
     $json_data = json_encode($result, JSON_PARTIAL_OUTPUT_ON_ERROR);
     echo $json_data;
 
-} catch(Exception $e){
-    $e->getMessage();
-	
+} catch(SoapFault $exception) {
+    echo $exception->getMessage();
+
 }
